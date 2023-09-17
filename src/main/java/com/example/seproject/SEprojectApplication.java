c378eb0b40a39fc2877952b0ad378006da9a26db
tonylzm <97021362+tonylzm@users.noreply.github.com>
2023-09-17 12:10:54 +0800

    第一次测试版本，无代码

---
+++ src/main/java/com/example/seproject/SEprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.seproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SEprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SEprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/seproject/SEprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.seproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SEprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
