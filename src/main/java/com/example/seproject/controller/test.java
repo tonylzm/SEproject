3716f5608f4e08664625eeae14cbfffa72a0ec7b
tonylzm <97021362+tonylzm@users.noreply.github.com>
2023-09-18 10:37:53 +0800

    设置启动页面

--- src/main/java/com/example/seproject/controller/test.java
+++ src/main/java/com/example/seproject/controller/test.java
@@ -1,13 +1,20 @@
 package com.example.seproject.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-@RestController
+import org.springframework.stereotype.Controller;
+@Controller
 public class test {
 
     @GetMapping("/")
     public String helloWorld() {
-        return "Hello, World!";
+        return "HW";//返回HW.html
     }
+    @RequestMapping("/ftlIndex")
+    public String ftlIndex() {
+        System.out.println("fff");
+        return "HW";
+    }
+
 }
