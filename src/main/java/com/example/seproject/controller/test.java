889010ffced51328b71fd135ef7faa1779a11af2
tonylzm <97021362+tonylzm@users.noreply.github.com>
2023-09-24 09:41:12 +0800

    生成唯一二维码

--- src/main/java/com/example/seproject/controller/test.java
+++ src/main/java/com/example/seproject/controller/test.java
@@ -1,9 +1,20 @@
 package com.example.seproject.controller;
 
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.stereotype.Controller;
+
+import java.io.IOException;
+import java.util.UUID;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+
 @Controller
 public class test {
 
@@ -16,5 +27,20 @@ public class test {
         System.out.println("fff");
         return "HW";
     }
+    @GetMapping("/qrcode")
+    public void generateQrcode(HttpServletResponse response) throws IOException, WriterException {
+        // 生成 UUID
+        UUID uuid = UUID.randomUUID();
+        String uuidStr = uuid.toString();
 
+        // 生成二维码
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+        int width = 300;
+        int height = 300;
+        BitMatrix bitMatrix = qrCodeWriter.encode(uuidStr, BarcodeFormat.QR_CODE, width, height);
+
+        // 将二维码写入响应流
+        response.setContentType("image/png");
+        MatrixToImageWriter.writeToStream(bitMatrix, "PNG", response.getOutputStream());
+    }
 }
