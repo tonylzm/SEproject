d74835d7639c5754355b21525698d95aabfe658b
tonylzm <97021362+tonylzm@users.noreply.github.com>
2023-10-26 21:58:27 +0800

    小程序部署docker初试

--- src/main/java/com/example/seproject/jpa/UserDao.java
+++ src/main/java/com/example/seproject/jpa/UserDao.java
@@ -6,5 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 public interface UserDao extends JpaRepository<User,Integer> {
+    User findByUsernameAndPassword(String username, String password);
 }
 //连接users表，并检查登录用户名和密码是否匹配
