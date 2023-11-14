7b6bacf4fed30fb6ac7e5be0c3458c80ed1f43b6
tonylzm <97021362+tonylzm@users.noreply.github.com>
2023-11-14 21:59:51 +0800

    加入权限检查

--- src/main/java/com/example/seproject/controller/StaffControll.java
+++ src/main/java/com/example/seproject/controller/StaffControll.java
@@ -5,16 +5,18 @@ import com.example.seproject.jpa.InStaffDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-
+import com.example.seproject.service.Check;
 @RestController
 public class StaffControll {
     @Autowired
     InStaffDao i;
+    @Autowired
+    Check check;
 
     @GetMapping("/loginstaff")//前端登录方法
-    public String loginstaff(@RequestParam("StaffIdcard")String StaffIdcard, @RequestParam("StaffPassword")String StaffPassword){
-        Internal_staff internal_staff=i. findByStaffIdcardAndStaffPassword(StaffIdcard,StaffPassword);
-        if(internal_staff!=null){
+    public String loginstaff(@RequestParam("StaffIdcard") String StaffIdcard, @RequestParam("StaffPassword") String StaffPassword) {
+        Internal_staff internal_staff = i.findByStaffIdcardAndStaffPassword(StaffIdcard, StaffPassword);
+        if (internal_staff != null) {
             return "登录成功";
         }
         return "登录失败";
@@ -22,7 +24,7 @@ public class StaffControll {
 
     @PostMapping("/addstaff") // 使用@PostMapping
     //此方法为向数据库中添加员工信息
-    public Internal_staff addStaff(@RequestBody Internal_staff request){
+    public Internal_staff addStaff(@RequestBody Internal_staff request) {
         // request对象包含了前端传入的数据
         request.setMainpower("系统管理员");
         Internal_staff savedStaff = i.save(request); // 假设VisitInfo类与数据库模型对应
@@ -30,36 +32,41 @@ public class StaffControll {
     }
 
     @PostMapping("/checkpower")
-    public String checkpower(@RequestParam("StaffIdcard")String StaffIdcard){
-        Internal_staff internal_staff=i.findByStaffIdcard(StaffIdcard);
-        String power=internal_staff.getStaffAffiliatedUnit()+internal_staff.getStaffPosition();
+    public String checkpower(@RequestParam("StaffIdcard") String StaffIdcard) {
+        Internal_staff internal_staff = i.findByStaffIdcard(StaffIdcard);
+        String power = internal_staff.getStaffAffiliatedUnit() + internal_staff.getStaffPosition();
         return power;
     }
 
     @GetMapping("/staffinfo")
-    public Internal_staff staffinfo(@RequestParam("StaffIdcard")String StaffIdcard){
-        Internal_staff internal_staff=i.findByStaffIdcard(StaffIdcard);
+    public Internal_staff staffinfo(@RequestParam("StaffIdcard") String StaffIdcard) {
+        Internal_staff internal_staff = i.findByStaffIdcard(StaffIdcard);
         internal_staff.setStaffPassword("*******");
         return internal_staff;
     }
 
     @GetMapping("/allstaff")
-    public Iterable<Internal_staff> allstaff(){
-        Iterable<Internal_staff> internal_staffs=i.findAll();
+    public Iterable<Internal_staff> allstaff() {
+        Iterable<Internal_staff> internal_staffs = i.findAll();
         return internal_staffs;
     }
 
     @GetMapping("/updatepower")
     public String updatepower(
-            @RequestParam("StaffIdcard")String StaffIdcard,
-            @RequestParam("mainpower")String mainpower,
-            @RequestParam("secondarypower")String secondarypower){
-        Internal_staff internal_staff=i.findByStaffIdcard(StaffIdcard);
-        internal_staff.setMainpower(mainpower);
-        internal_staff.setSecondarypower(secondarypower);
-        i.save(internal_staff);
-        return "修改成功";
+            @RequestParam("edit") String edit,
+            @RequestParam("StaffIdcard") String StaffIdcard,
+            @RequestParam("mainpower") String mainpower,
+            @RequestParam("secondarypower") String secondarypower) {
+        if (check.checkpower(edit).contains("主管")) {
+            {
+                Internal_staff internal_staff = i.findByStaffIdcard(StaffIdcard);
+                internal_staff.setMainpower(mainpower);
+                internal_staff.setSecondarypower(secondarypower);
+                i.save(internal_staff);
+                return "修改成功";
+            }
+
+        }
+        return "权限不足";
     }
-
-
 }
