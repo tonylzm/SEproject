6c77a221f7ff3d8fc0533cb00bd8682f613ec286
tonylzm <97021362+tonylzm@users.noreply.github.com>
2023-11-25 09:58:26 +0800

    修复了一些显示bug

--- src/main/java/com/example/seproject/controller/VisitinfoControll.java
+++ src/main/java/com/example/seproject/controller/VisitinfoControll.java
@@ -270,6 +270,7 @@ public class VisitinfoControll {
         blockInfo.setVisitorName(request.getVisitorName());
         blockInfo.setAffiliatedUnit(request.getAffiliatedUnit());
         blockInfo.setBlocktime(request.getBlocktime());
+        blockInfo.setBlockpeople("管理员");
         visitinfo visitor = v.findByVisitorPhone(request.getVisitorPhone());
         if (visitor != null) {
             // 修改 applicationStatus 的值为 "拉黑"
